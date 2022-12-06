bcfc487cf65d41924e39ee94c41faeeb401691e9
Chuah Tse Yung <chuahtseyung2002@gmail.com>
2022-12-06 15:39:58 +0800

    removed door outline

--- Cub3D/srcs/c3d_door.c
+++ Cub3D/srcs/c3d_door.c
@@ -6,7 +6,7 @@
 /*   By: schuah <schuah@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 16:14:49 by schuah            #+#    #+#             */
-/*   Updated: 2022/11/29 16:22:32 by schuah           ###   ########.fr       */
+/*   Updated: 2022/12/06 15:33:59 by schuah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,16 +34,36 @@ static void	fade_door(t_gm *gm)
 	}
 }
 
+static void	change_char(t_gm *gm, char from, char to)
+{
+	t_ivct	pos;
+
+	pos.y = -1;
+	while (++pos.y < gm->map.size.y)
+	{
+		pos.x = -1;
+		while (++pos.x < gm->map.size.x)
+		{
+			if (gm->map.map[pos.y][pos.x] == from)
+				gm->map.map[pos.y][pos.x] = to;
+		}
+	}
+}
+
 void	c3d_update_door(t_gm *gm)
 {
 	static int	playback = 1;
 
 	if (gm->win.playing == 0)
 		return ;
+	if (gm->map.door_state == D_OPEN)
+		change_char(gm, 'X', 'D');
 	gm->win.frame += playback;
 	fade_door(gm);
 	if (gm->win.frame >= DOOR_SPD || gm->win.frame <= 0)
 	{
+		if (gm->map.door_state == D_CLOSE)
+			change_char(gm, 'D', 'X');
 		gm->win.playing = 0;
 		playback *= -1;
 	}
