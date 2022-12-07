6aaaa0fffa361c7d1fc5383e121b8573e1cf9ffe
Chuah Tse Yung <chuahtseyung2002@gmail.com>
2022-12-07 18:01:33 +0800

    Added more maps and removed leaks

--- Cub3D/srcs/c3d_exit.c
+++ Cub3D/srcs/c3d_exit.c
@@ -6,7 +6,7 @@
 /*   By: schuah <schuah@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 16:16:44 by schuah            #+#    #+#             */
-/*   Updated: 2022/11/23 16:29:39 by schuah           ###   ########.fr       */
+/*   Updated: 2022/12/07 17:57:14 by schuah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,10 @@
 
 int	c3d_fail_exit(char *msg, char *details)
 {
-	ft_dprintf(2, "Error\n");
-	ft_dprintf(2, "%s", msg);
+	ft_dprintf(2, "Error\n%s", msg);
 	if (details != NULL)
 		ft_dprintf(2, ": |%s|", details);
 	ft_dprintf(2, "\n");
-	system("leaks -q cub3D");
 	exit(1);
 	return (1);
 }
@@ -27,7 +25,6 @@ int	c3d_fail_exit(char *msg, char *details)
 int	c3d_success_exit(void)
 {
 	printf("Thanks for playing!\n");
-	system("leaks -q cub3D");
 	exit(0);
 	return (0);
 }
